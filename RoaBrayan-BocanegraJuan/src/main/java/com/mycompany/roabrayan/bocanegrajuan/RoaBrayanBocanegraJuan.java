212b4cce77ff99e17bcd750195a752574cf2e02a
sala8 <sala8@is-ls08e10.FI-LabSistemas.ucatolica.loc>
2023-09-04 21:09:04 -0500

    Primer commit

---
+++ RoaBrayan-BocanegraJuan/src/main/java/com/mycompany/roabrayan/bocanegrajuan/RoaBrayanBocanegraJuan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.roabrayan.bocanegrajuan;
+
+/**
+ *
+ * @author sala8
+ */
+public class RoaBrayanBocanegraJuan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
